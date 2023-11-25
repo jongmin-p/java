49df7dbcee9fd9b072b1847f24503100fdf136f0
jongmin-p <sjttk123@gmail.com>
2023-11-24 21:06:53 -0500

    create NGS directory and the first commit

---
+++ NGS/ch05/src/Ex5_1_tmp.java
@@ -0,0 +1,19 @@
+
+public class Ex5_1_tmp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] score = new int[5];
+		score[3] = 100;
+		
+		System.out.println("score[0] = " + score[0]);
+		System.out.println("score[1] = " + score[1]);
+		System.out.println("score[2] = " + score[2]);
+		System.out.println("score[3] = " + score[3]);
+		System.out.println("score[4] = " + score[4]);
+		System.out.println();
+		
+		int value = 100;
+		System.out.println("value = " + value);
+	}
+}
