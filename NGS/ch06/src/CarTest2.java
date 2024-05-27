48e9194ca897a0d1ba4d3fc3db529d4e5d429832
jongmin-p <sjttk123@gmail.com>
2024-05-27 10:34:32 +0900

    this & this()

---
+++ NGS/ch06/src/CarTest2.java
@@ -0,0 +1,35 @@
+public class CarTest2 {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        Car c2 = new Car("blue");
+
+        System.out.println("c1 의 color = " + c1.color
+                + ", gearType = " + c1.gearType
+                + ", door = " + c1.door);
+
+        System.out.println("c2 의 color = " + c2.color
+                + ", gearType = " + c2.gearType
+                + ", door = " + c2.door);
+    }
+}
+
+
+class Car {
+    String color;
+    String gearType;
+    int door;
+
+    Car() {
+        this("white", "auto", 4);
+    }
+
+    Car(String color) {
+        this(color, "auto", 4);
+    }
+
+    Car(String color, String gearType, int door) {
+        this.color = color;
+        this.gearType = gearType;
+        this.door = door;
+    }
+}
